e03208009bf7583fb3f635622ff4db9afc98f080
au.zhurov <alexzhurovbiz@gmail.com>
2022-11-01 21:58:55 +0200

    added table component

---
+++ client/src/components/Table/Table.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react';
+
+const Table: FunctionComponent = () => (
+    <table>
+        <tbody>
+            <tr>
+                <td>Lorem ipsum</td>
+            </tr>
+        </tbody>
+    </table>
+);
+
+export default Table;
